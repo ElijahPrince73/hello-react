1aca6f57aca6db335dadc6e27ae4b74c3c90f9df
elijahprince73 <elijahprince73@gmail.com>
2017-05-09 08:41:43 -0700

    added props

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,24 @@
 var Greeter = React.createClass({
+ getDefaultProps: function() {
+  return {
+   name: 'React',
+   message: 'This is a default Message'
+  };
+ },
  render: function() {
+  var name = this.props.name
+  var message = this.props.message
   return (
    <div>
-     <h1>Hello React</h1>
-    <p>THis is From the Componet</p>
+     <h1>Hello {name}</h1>
+    <p>{message + '!!'}</p>
    </div>
   );
  }
 })
 
+var firstName = 'Elijah'
+
 ReactDOM.render(
- <Greeter/>,
+ <Greeter name={firstName} message="This is from the prop"/>,
  document.getElementById('app'));
