9d062017aef2df3b24df763e3df72709f489065b
elijahprince73 <elijahprince73@gmail.com>
2017-05-09 08:49:06 -0700

    event and callback

--- public/app.jsx
+++ public/app.jsx
@@ -5,13 +5,24 @@ var Greeter = React.createClass({
    message: 'This is a default Message'
   };
  },
+ onButtonClick: function(e) {
+  e.preventDefault();
+
+  var name = this.refs.name.value;
+  alert(name)
+ },
  render: function() {
   var name = this.props.name
   var message = this.props.message
   return (
    <div>
      <h1>Hello {name}</h1>
-    <p>{message + '!!'}</p>
+     <p>{message + '!!'}</p>
+
+    <form onSubmit={this.onButtonClick}>
+     <input type="text" ref="name"></input>
+    <button>Set Name</button>
+    </form>
    </div>
   );
  }
